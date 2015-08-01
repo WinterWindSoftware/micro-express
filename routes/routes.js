54dd832f617ef1ed1c98e5df65dca977d1e76ba0
Paul Swail <paul@winterwindsoftware.com>
2015-08-01 19:37:35 +0100

    500 error handler

--- routes/routes.js
+++ routes/routes.js
@@ -1,3 +1,5 @@
+import logger from '../components/app-logger';
+
 export default (app) => {
     app.use('/system', require('./system/system.controller'));
 
@@ -9,6 +11,15 @@ export default (app) => {
 
     // All undefined asset or api routes should return a 404
     //app.route('/:url(api|auth|components|app|bower_components|assets)/*').get(errors[404]);
+
+    //Catch-all error handler
+    app.use((err, req, res, next) => {
+        logger.error('Unhandled server error', err);
+        res.status(500).json({
+            error: 'An unexpected error occurred'
+        });
+    });
+
 
     //unhandled errors
     //app.use(errors.unhandledError);
