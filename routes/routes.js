1c26d513d3ae39d120c76c88b9e9656b2559a795
Paul Swail <paul@winterwindsoftware.com>
2015-08-01 19:44:03 +0100

    404 handler

--- routes/routes.js
+++ routes/routes.js
@@ -1,26 +1,25 @@
 import logger from '../components/app-logger';
 
 export default (app) => {
-    app.use('/system', require('./system/system.controller'));
-
+    // STANDARD ROUTE HANDLERS
     app.get('/', (req, res) => {
         res.json({
             msg: 'Welcome!'
         });
     });
+    app.use('/system', require('./system/system.controller'));
 
-    // All undefined asset or api routes should return a 404
-    //app.route('/:url(api|auth|components|app|bower_components|assets)/*').get(errors[404]);
+    // ERROR HANDLERS (these must be wired up last)
+    // 404
+    app.use((req, res) => {
+        res.status(404).json({error: 'You took the wrong route, bud!'});
+    });
 
-    //Catch-all error handler
+    // 500 - Catch-all error handler
     app.use((err, req, res, next) => {
         logger.error('Unhandled server error', err);
         res.status(500).json({
             error: 'An unexpected error occurred'
         });
     });
-
-
-    //unhandled errors
-    //app.use(errors.unhandledError);
 }
